5bb6ef3367a7557d084e13cb29c022ac0c9bbaa9
Prathap M <www.prathu4413@gmail.com>
2025-11-11 16:40:37 +0530

    changes in workflow

---
+++ fullstack-main/frontend/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/api/message")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(() => setMessage("Error connecting to backend"));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Frontend + Backend Communication</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
